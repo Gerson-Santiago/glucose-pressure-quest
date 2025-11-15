6d4ecf77bc0d5f80d70e472b4c8e7e361a5492d1
Gerson Santiago <gersondossantossantiago@gmail.com>
2025-11-15 14:50:28 -0300

    Atualização e formatação dos arquivos de tsconfig e vite config

--- vite.config.ts
+++ vite.config.ts
@@ -17,31 +17,3 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
-
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
-// import path from "path";
-// import { componentTagger } from "lovable-tagger";
-
-// // https://vitejs.dev/config/
-// export default defineConfig(({ mode }) => ({
-//   // 🔹 base: "/" para dev, "/glucose-pressure-quest/" para produção (GitHub Pages)
-//   base: mode === "production" ? "/glucose-pressure-quest/" : "/",
-  
-//   server: {
-//     host: "::",
-//     port: 8080,
-//   },
-  
-//   plugins: [
-//     react(),
-//     mode === "development" && componentTagger(),
-//   ].filter(Boolean),
-  
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// }));
